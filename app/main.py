561ab0e96154ce4c77862d22169743ad2bef6431
tippy3 <26988793+tippy3@users.noreply.github.com>
2023-09-03 18:15:53 +0900

    update lambda handler

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import cloudwatch
 import switchbot
 
 
-def main():
+def lambda_handler(event, context):
   data = switchbot.get_data()
   temperature = data['temperature']
   humidity = data['humidity']
@@ -12,4 +12,4 @@ def main():
 
 
 if __name__ == "__main__":
-  main()
+  lambda_handler(None, None)
